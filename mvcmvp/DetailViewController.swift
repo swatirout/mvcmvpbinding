12eab0ec63f06150b3cb7fee09f5bcba8bbc21aa
Swati Rout <swati.rout@clearesult.com>
2020-06-26 12:35:32 +0530

    Initial Commit

---
+++ mvcmvp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  mvcmvp
+//
+//  Created by Swati Rout on 26/06/20.
+//  Copyright © 2020 Swati Rout. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
